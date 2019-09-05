7823426991b18fd751aa9916a50d0056c0efcb75
wujian <zkwujian@163.com>
2019-09-05 15:04:15 +0800

    1.增加参数设置脚本

--- obos_setparam.py
+++ obos_setparam.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from time import sleep
+
 #修改项
 DevNumList="1903300540"
+ParamIdList="0x030000D9"
 
 # 打开Chrome浏览器
 browser = webdriver.Chrome()
@@ -22,17 +26,44 @@ browser.find_element_by_id("taskdfdiv").click()
 #切换窗口
 browser.switch_to.window(browser.window_handles[1])
 #browser.implicitly_wait(1)
-browser.switch_to.frame(0)
-
-#进入参数设置
-browser.find_element_by_link_text("02-设置终端参数").click()
-
-#填充终端号
-browser.switch_to.default_content()
-browser.switch_to.frame("tdMainPage")
-browser.find_element_by_name("terminalID").send_keys(DevNumList)
 
 
+
+for i in range(2):
+    #进入参数设置
+    browser.switch_to.default_content()
+    browser.switch_to.frame("left")
+    browser.find_element_by_link_text("02-设置终端参数").click()
+
+    #填充终端号
+    browser.switch_to.default_content()
+    browser.switch_to.frame("tdMainPage")
+    browser.find_element_by_name("terminalID").send_keys(DevNumList)
+    browser.find_element_by_name("paramType").send_keys(ParamIdList)
+    #填充参数类型
+    s1 = Select(browser.find_element_by_name("paramFormat"))  # 实例化Select
+    s1.select_by_value("DWord")
+    #填充参数值
+    print("val=%d"%i)
+    browser.find_element_by_name("paramValue").send_keys(i)
+    #添加
+    browser.find_element_by_xpath('//*[@id="Tbl"]/thead/tr[7]/td/input').click()
+    browser.implicitly_wait(1)
+    #提交
+    browser.find_element_by_xpath('//*[@id="Tbl"]/thead/tr[8]/td[1]/input').click() 
+    
+    browser.switch_to.default_content()
+    browser.switch_to.frame("left")
+    browser.find_element_by_link_text("01-查询任务进度").click()
+    
+    browser.switch_to.default_content()
+    browser.switch_to.frame("tdMainPage")
+    browser.find_element_by_name("terminal_id").send_keys(DevNumList)
+    browser.find_element_by_xpath('//*[@id="Tbl"]/thead/tr[4]/td/input').click()
+    
+    sleep(10)
+    browser.find_element_by_xpath('//*[@id="Tbl"]/thead/tr[4]/td/input').click()
+    sleep(2)
 
 
 '''

e5f52b77b1c6f95c543fa56a625fd2d350dee44e
wujian <zkwujian@163.com>
2019-10-24 21:46:33 +0800

    增加ox01数据

--- parse_gb_from_flash.py
+++ parse_gb_from_flash.py
@@ -25,9 +25,12 @@ class GbParse0x05:
         idx += 4
         self.__lat = struct.unpack(">I", data[idx:idx+4])[0]
         idx += 4
-        
+
+        len[0] = idx
+
     def display(self):
-        print("0x05: location=%d, lat=%f, longi=%f" %(self.__locationed, self.__lat/1e6, self.__longi/1e6))
+        print("车辆位置: location=%d, lat=%f, longi=%f" %(self.__locationed, self.__lat/1e6, self.__longi/1e6))
+
 # 报警数据
 class GbParse0x07:
     
@@ -75,9 +78,40 @@ class GbParse0x07:
                 self.__other_fault_list.append(struct.unpack(">I", data[idx:idx+4])[0])
                 idx += 4
         
+        len[0] = idx
+
     def display(self):
-        print("0x07: fault level=%d, common fault=0x%X, bms=%d, monitor=%d, engine=%d, other=%d" 
+        print("报警数据: fault level=%d, common fault=0x%X, bms=%d, monitor=%d, engine=%d, other=%d" 
             %(self.__fault_level, self.__common_fault, self.__bms_fault_num, self.__monitor_fault_num, self.__engine_fault_num, self.__other_fault_num))
+
+# 整车数据
+class GbParse0x01:
+    
+    def __init__(self, s, len):
+        data = s
+        idx = 0
+        #print(data[idx:])
+        (self.__vehicle_stat,
+        self.__charge_stat, 
+        self.__RunMode, 
+        self.__speed, 
+        self.__odo,
+        self.__tot_volt, 
+        self.__tot_curr, 
+        self.__soc,
+        self.__dcdc_stat,
+        self.__gear,
+        self.__InsResistance,
+        self.__accel_pedal,
+        self.__brake_pedal) = struct.unpack(">BBBHIHHBBBHBB", data[idx:idx+20])
+        idx += 20
+        len[0] = idx
+
+    def display(self):
+        print("整车数据: 车辆状态:%d, 充电状态:%d, 运行模式:%d, 车速:%d, 累计里程:%d, 总电压:%d, 总电流:%d, SOC:%d, DCDC:%d, 档位:%d, 绝缘电阻:%d, 加速踏板:%d, 制动踏板:%d"
+            %(self.__vehicle_stat, self.__charge_stat, self.__RunMode, self.__speed, self.__odo, self.__tot_volt, 
+            self.__tot_curr, self.__soc, self.__dcdc_stat, self.__gear, self.__InsResistance, self.__accel_pedal, self.__brake_pedal))
+
 
 class GbMainParse:
     # 数据采集时间
@@ -97,23 +131,24 @@ class GbMainParse:
         print(self.__time)
         
         # 解析各个数据块
+        used_len = [0]
         while len > 0:
             self.__block_id = struct.unpack("<B", data[idx:idx+1])[0]
             idx += 1
             print("__block_id=", self.__block_id)
             if self.__block_id == 0x05:
-                self.__block = GbParse0x05(data[idx:], len)
-                idx += 9
-                len -= 9
+                self.__block = GbParse0x05(data[idx:], used_len)
             elif self.__block_id == 0x07:
-                self.__block = GbParse0x07(data[idx:], len)
-                idx += 9
-                len -= 9
+                self.__block = GbParse0x07(data[idx:], used_len)
             elif self.__block_id == 0x01:
-                self.__block = GbParse0x07(data[idx:], len)
-                idx += 9
-                len -= 9
+                self.__block = GbParse0x01(data[idx:], used_len)
+            elif self.__block_id == 0x02:
+                #self.__block = GbParse0x01(data[idx:], used_len)
                 break
+
+            print("used_len=", used_len)
+            idx += used_len[0]
+            len -= used_len[0]
             self.__block.display()
             
         # 校验码 TODO

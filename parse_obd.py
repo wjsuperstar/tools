fe22219062236585aa0b3539020a925db6ccebb5
wujian <zkwujian@163.com>
2020-01-04 23:09:01 +0800

    1.增加对json格式数据的解析脚本。。。。。。

---
+++ parse_obd.py
@@ -0,0 +1,54 @@
+import re
+import sys
+import json
+
+print("使用格式： python xx.py 文件名")
+print("param:", sys.argv[1])
+
+FileName = sys.argv[1]
+ErrorList = []
+
+def get_all_dict(data):
+    if isinstance(data,list):
+        data = data[0]
+    if isinstance(data,dict):
+        for x in range(len(data)): 
+            temp_key = list(data.keys())[x] 
+            temp_value = data[temp_key]
+            if not (isinstance(temp_value, list) or isinstance(temp_value, dict)):
+                #print('{} : {}'.format(temp_key,temp_value))
+                print('%-8s : %-32s' % (temp_key, temp_value), end=';')
+            
+            get_all_dict(temp_value) # 迭代输出结果
+
+def parse_file():
+    with open(FileName, encoding='utf-8') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            #data = re.search('(?<=Boby:\[)(.*)(?=\],\sHttpRecv)', line).group(1)
+            #data2 = re.search('(?<=HttpRecv:\[)(.*)', line).group(1)
+            devid = re.search('DevID:(.*?),', line).group(1)
+            #print('devid=', devid)
+            data = re.search('Boby:\[(.*)\],\sHttpRecv:\[(.*)\]', line)
+            js1 = json.loads(data.group(1))
+            js2 = json.loads(data.group(2))
+            ss1=js2['body'][0]['error']
+            if ss1[0:4] == '[Wd]' or ss1[0:4] == '[Jd]' :
+                print('DevId=', devid, ss1, 'JD=', js1['body']['JD'], 'WD=', js1['body']['WD'])
+            
+            #print(ss1)
+            #ErrorList.append(ss1)
+            
+            #get_all_dict(js1)
+            #get_all_dict(js2)
+            #print('\n')
+            #break;
+if __name__ == '__main__':
+    parse_file()
+    '''
+    s1= set(ErrorList)
+    for i in s1:
+        print(i)
+    '''
